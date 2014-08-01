91f140db3247f5746248dfe0bbb8329095980bf2
NARUSE, Yui <naruse@airemix.jp>
2014-08-02 04:12:28 +0900

    skip if dummy encodings
    dummy encodings like
    * UTF-16 has BOM. Note that
    * UTF-32: ditto.
    * ISO-2022-JP: a stateful encoding
    are hard to use as a internal encoding.

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -5,6 +5,7 @@ require_relative '../lib/regexp_m17n'
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
+      next if enc.dummy?
       str = '.'.encode(enc) rescue next
       assert(RegexpM17N.non_empty?(str))
     end
