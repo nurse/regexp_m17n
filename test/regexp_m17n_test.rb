3f89de132b12c8a1cbf49b035fbbb9e7febb370e
NARUSE, Yui <naruse@airemix.jp>
2014-08-02 03:52:29 +0900

    skip if Converter is not found

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -5,7 +5,8 @@ require_relative '../lib/regexp_m17n'
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
-      assert(RegexpM17N.non_empty?('.'.encode(enc)))
+      str = '.'.encode(enc) rescue next
+      assert(RegexpM17N.non_empty?(str))
     end
   end
 end
